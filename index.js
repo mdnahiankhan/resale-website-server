c5504682d339f2485be655f88343743c420339f6
Md. Nahian <102530128+mdnahiankhan@users.noreply.github.com>
2022-11-24 23:35:52 +0600

    added category

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -18,12 +18,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const productsCollection = client.db('resalesDokan').collection('products');
-        const productsBrandCollection = client.db('resalesDokan').collection('brandwise');
+        const optionsBrandCollection = client.db('resalesDokan').collection('options');
         app.get('/products', async (req, res) => {
             const query = {}
             const product = await productsCollection.find(query).toArray();
             res.send(product);
         })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await productsCollection.find(filter).toArray();
+            res.send(result)
+        })
+        app.get('/options', async (req, res) => {
+            const query = {}
+            const option = await optionsBrandCollection.find(query).toArray();
+            res.send(option);
+        })
+        // app.get('/options/:id', async (req, res) => {
+        //     const id = req.params.id
+        //     const filter = { category_id: id }
+        //     const result = await optionsBrandCollection.findOne(filter)
+        //     res.send(result)
+        // })
+
+
     }
     finally {
 
@@ -31,6 +50,16 @@ async function run() {
 }
 run().catch(console.log)
 
+const options = require('./data/catagories.json')
+
+app.get('/options', (req, res) => {
+    res.send(options)
+})
+app.get('/options/:id', async (req, res) => {
+    const id = req.params.id
+    const category_id = options.filter(option => option.category_id === id);
+    res.send(category_id)
+})
 
 app.get('/', async (req, res) => {
     res.send('Resale website is running')
