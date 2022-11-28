a49e6c145ac984f56e948f80e2a0b93eb2d8446d
Md. Nahian <102530128+mdnahiankhan@users.noreply.github.com>
2022-11-28 21:28:08 +0600

    added put and delete

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 const app = express();
 
@@ -40,10 +40,10 @@ async function run() {
         const usersCollection = client.db('resalesDokan').collection('users');
         const sellersCollection = client.db('resalesDokan').collection('sellers');
         const addproductsCollection = client.db('resalesDokan').collection('addproducts');
+        const paymentsCollection = client.db('resalesDokan').collection('payments');
 
         /* verify admin */
         const verifyAdmin = async (req, res, next) => {
-            console.log('inside the verify admin', req.decoded.email);
             const decodedEmail = req.decoded.email;
             const query = { email: decodedEmail };
             const sellers = await sellersCollection.findOne(query);
@@ -138,14 +138,62 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const booking = await bookingsCollection.findOne(query);
+            res.send(booking);
+        })
+
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await bookingsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.carPrice;
+            const amount = price * 10;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ],
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        })
+
+        app.post('/payments', async (req, res) => {
+            const payment = req.body
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.dataId;
+            const filter = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transectionId: payment.transectionId
+                }
+            }
+            const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+            res.send(result)
+        })
+
         /* users collection api and jwt token */
 
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
-            const user = await usersCollection.findOne(query);
+            const user = await sellersCollection.findOne(query);
+
             if (user) {
-                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1d' });
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '2d' });
                 return res.send({ accessToken: token })
             }
             res.status(403).send({ accessToken: '' });
@@ -161,6 +209,13 @@ async function run() {
             const user = req.body;
             const result = await usersCollection.insertOne(user)
             res.send(result)
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await usersCollection.deleteOne(filter);
+            res.send(result);
         })
 
         /* sellers api  */
@@ -184,6 +239,13 @@ async function run() {
             res.send(resutls)
         })
 
+        app.get('/sellers/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const seller = await sellersCollection.findOne(query);
+            res.send({ isSeller: seller?.role === "seller" })
+        })
+
         app.put('/sellers/admin/:id', verifyJwt, async (req, res) => {
             const decodedEmail = req.decoded.email;
             const query = { email: decodedEmail };
@@ -201,6 +263,28 @@ async function run() {
             }
             const result = await sellersCollection.updateOne(filter, updatedDoc, options);
             res.send(result)
+        })
+
+        app.put('/sellers/verified/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    role: "verified"
+                }
+            }
+            const result = await sellersCollection.updateOne(filter, updatedDoc, options)
+            res.send(result);
+        })
+
+
+
+        app.delete('/sellers/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await sellersCollection.deleteOne(filter)
+            res.send(result);
         })
 
     }
